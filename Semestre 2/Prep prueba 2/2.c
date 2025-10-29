0fb997b8c95ad53802f3e724b3a0fb7c98ea2b48
C <claudiolorenzosb@gmail.com>
2025-10-29 15:39:43 -0300

    First Commit

--- Semestre 2/Prep prueba 2/2.c
+++ Semestre 2/Prep prueba 2/2.c
@@ -13,23 +13,19 @@ char laberinto[FILAS][COLUMNAS] = {
     {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' '},
     {'#', '#', '#', '#', '#', ' ', '#', ' ', '#', ' '},
     {' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' '},
-    {' ', '#', '#', '#', '#', ' ', '#', '#', '#', ' '},
-    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' '},
+    {' ', '#', '#', '#', '#', ' ', '#', ' ', '#', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
     {'#', '#', '#', '#', '#', '#', '#', '#', '#', 'B'}};
 // Prototipos de funciones
 void mostrarlaberinto();
 int calcularRutas(char laberinto[FILAS][COLUMNAS],int x,int y);
+int resultadolab(char laberinto[FILAS][COLUMNAS]);
 
 int main(){
     printf("----- BUSCANDO EL CAMINO DEL laberinto RECURSIVO ---\n");
     printf("Buscando el camino desde A hasta B \n");
     mostrarlaberinto();
-    if(calcularRutas(laberinto, INICIOx,INICIOy)){
-        printf("Hecho\n");
-        mostrarlaberinto();
-    } else {
-        printf("Sin camino\n");
-    }
+    resultadolab(laberinto);
     return 0;
 
 }
@@ -53,26 +49,30 @@ int calcularRutas(char laberinto[FILAS][COLUMNAS], int x,int y){
     }
     laberinto[x][y] = 'c';
     //movimientos
-    // Mover hacia abajo
     if (x + 1 < FILAS && (laberinto[x + 1][y] == ' ' || laberinto[x + 1][y] == 'B')) {
         if (calcularRutas(laberinto, x + 1, y)) return 1;
     }
 
-    // Mover hacia arriba
     if (x - 1 >= 0 && (laberinto[x - 1][y] == ' ' || laberinto[x - 1][y] == 'B')) {
         if (calcularRutas(laberinto, x - 1, y)) return 1;
     }
 
-    // Mover hacia la derecha
     if (y + 1 < COLUMNAS && (laberinto[x][y + 1] == ' ' || laberinto[x][y + 1] == 'B')) {
         if (calcularRutas(laberinto, x, y + 1)) return 1;
     }
 
-    // Mover hacia la izquierda
     if (y - 1 >= 0 && (laberinto[x][y - 1] == ' ' || laberinto[x][y - 1] == 'B')) {
         if (calcularRutas(laberinto, x, y - 1)) return 1;
     }
 
     laberinto[x][y] = ' ';
     return 0;
+}
+int resultadolab(char laberinto[FILAS][COLUMNAS]){
+    if(calcularRutas(laberinto, INICIOx,INICIOy)){
+        printf("Hecho\n");
+        mostrarlaberinto();
+    } else {
+        printf("Sin camino\n");
+    }
 }
